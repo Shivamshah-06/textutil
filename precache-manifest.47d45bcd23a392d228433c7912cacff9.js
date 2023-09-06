2c2207ffae39eb82e5a81a1d93d98cefcdf286fb
Shivamshah-06 <shivamshah93865@gmail.com>
2023-09-06 23:40:35 +0530

    Updates

---
+++ precache-manifest.47d45bcd23a392d228433c7912cacff9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2354d3bf46546d3380a",
+    "url": "/textutil/static/css/main.cfe336ce.chunk.css"
+  },
+  {
+    "revision": "c2354d3bf46546d3380a",
+    "url": "/textutil/static/js/main.8b45bb60.chunk.js"
+  },
+  {
+    "revision": "3ee96023870a4d7c0f35",
+    "url": "/textutil/static/js/runtime~main.8f86f551.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/textutil/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/textutil/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "56f49f2f9a768e18b90ae5d2db6b6e2a",
+    "url": "/textutil/index.html"
+  }
+];
